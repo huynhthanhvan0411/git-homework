2cae78699ff5e2b9cc5cd831147115b97675b643
thanhvan <huynhthanhvan0411@gmail.com>
2024-04-23 22:41:12 +0700

    change feature A

--- example.js
+++ example.js
@@ -2,6 +2,13 @@ const a = 1;
 const b = 2;
 const c = 3;
 
-function master () {
-    return a + b + c;
+function master() {
+  return a + b + c;
+}
+function branchCommon() {
+  if (a === 1) {
+    return a * b * c;
+  }
+
+  return 0;
 }
