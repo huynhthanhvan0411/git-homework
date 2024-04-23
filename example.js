0be6b14117ee3531133573e10598516bc091f8b8
thanhvan <huynhthanhvan0411@gmail.com>
2024-04-23 22:43:37 +0700

    change B

--- example.js
+++ example.js
@@ -5,9 +5,10 @@ const c = 3;
 function master() {
   return a + b + c;
 }
+
 function branchCommon() {
-  if (a === 1) {
-    return a * b * c;
+  if (c === 3) {
+    return c - b - a;
   }
 
   return 0;
